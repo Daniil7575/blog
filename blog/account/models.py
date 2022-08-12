b8682705ff39da77bb972a500c7a0f78aa11cac7
Daniil7575 <spider2013vip@mail.ru>
2022-08-12 15:27:12 +0300

    Profile model

--- blog/account/models.py
+++ blog/account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    profile_image = models.ImageField(verbose_name='Картинка профиля', upload_to="users/%Y/%m/%d", blank=True)
+    date_of_birt = models.DateTimeField(blank=True, verbose_name="Дата рождения")
+
+    def __str__(self) -> str:
+        return f'Профиль {self.user.username}'
+    
