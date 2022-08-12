ab2a836f0126d830cfd5bf8bb0d4dd92841d2611
Daniil7575 <spider2013vip@mail.ru>
2022-08-12 16:55:03 +0300

    Bootstrap implementation and header/navbar

--- blog/account/urls.py
+++ blog/account/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views
+from .views import *
 
 
 urlpatterns = [
-    
+    path('authentication/', views.LoginView.as_view(), name='login'),
+    path('', mainpage, name='mainpage')
 ]
