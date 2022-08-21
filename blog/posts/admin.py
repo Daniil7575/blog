6a6610ad307824878798014ec2a8a29c00532afa
Daniil7575 <spider2013vip@mail.ru>
2022-08-22 00:51:34 +0300

    Recent activity

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -8,4 +8,5 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
 # Register your models here.
